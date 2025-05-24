44373f35c2faa63233e627c96614a8744e6aba28
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-24 19:04:51 -0300

    finish aula26

---
+++ Módulo 01/aula26/Online PHP/op3.php
@@ -0,0 +1,10 @@
+<?php 
+    $add = 5 + 2;
+    $sub = 5 - 2;
+    $mult = 5 * 2;
+    $divint = 5 / 2;
+    $divres = 5 % 2;
+    $expo = 5 ** 2;
+
+    echo "A adição é $add | A subtração é $sub | A Multiplicação é $mult | A divisão inteira é $divint | A divisão real é $divres | A exponenciação é $expo"
+?>
